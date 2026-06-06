0aefce5d0974eb14015f44d685a8d925f0f27f12
Konstantin Naumenko <whostolemysleep@gmail.com>
2026-06-06 20:49:53 +0700

    fix

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,13 @@
 import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
+import type { NeonHttpDatabase } from 'drizzle-orm/neon-http'
 import * as schema from './schema'
 
-const sql = neon(process.env.POSTGRES_PRISMA_URL!)
-export const db = drizzle(sql, { schema })
+let _db: NeonHttpDatabase<typeof schema> | undefined
+
+export const db = new Proxy({} as NeonHttpDatabase<typeof schema>, {
+  get(_, prop) {
+    _db ??= drizzle(neon(process.env.POSTGRES_PRISMA_URL!), { schema })
+    return Reflect.get(_db, prop)
+  },
+})
